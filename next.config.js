7b240adae3e715675ae73423a24232178cf6d52b
ceedev23 <ceedev23@gmail.com>
2025-03-30 23:27:41 -0400

    Update: Configure Next.js for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/My-Blog',
-  assetPrefix: '/My-Blog/',
-  trailingSlash: true,
-  distDir: 'out',
 }
 
 module.exports = nextConfig 
